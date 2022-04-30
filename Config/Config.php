24be2807598454dc0631689053f333fafafb0a58
jasonrivera96 <jason-five@hotmail.com>
2022-04-30 01:29:10 -0500

    Ecommerce Commit Inicial

---
+++ Config/Config.php
@@ -0,0 +1,23 @@
+<?php 
+	
+	/* const BASE_URL = "https://ecommerce.com/tienda"; */
+	const BASE_URL = "http://localhost/ecommerce";
+
+	//Zona horaria
+	date_default_timezone_set('America/Guayaquil');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_ecommerce";
+	const DB_USER = "root";
+	const DB_PASSWORD = "12345";
+	const DB_CHARSET = "charset=utf8";
+
+	//Delimitadores decimales/millares
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+
+ ?>
